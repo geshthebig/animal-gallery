634e289db3d0b14b17f7451e2500b013012def8e
geshthebig <geshthebig@yahoo.com>
2020-01-28 11:10:13 +0200

    Add images ot html

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Animal Gellery</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Animal Gellery</h1>
+
+        <div class="gallery"> 
+            <img src="Images/Monkey1.jpg" alt="Monkey">
+            <img src="Images/Monkey2.jpg" alt="Monkey">
+            <img src="Images/Hourses.jpg" alt="Hourses">
+            <img src="Images/Lion.jpg" alt="Lion">
+            <img src="Images/Rabbit.jpg" alt="Rabbit">
+        </div>
+
+    </div>
+</body>
+</html>
